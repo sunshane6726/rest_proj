55d2c29bbaca850f8b33225c6edea1369988b2d9
sunshane6726 <sunshane6726@naver.com>
2019-10-08 13:21:38 +0900

    proj Action Post=Essay

--- mystorage/views.py
+++ mystorage/views.py
@@ -1,10 +1,34 @@
 #from django.shortcuts import render 사용 안하구요
 from rest_framework import viewsets
 from .models import Essay
-#from .serializers import EssaySerializer
+from .serializers import EssaySerializer
 # Create your views here.
+from rest_framework.filters import SearchFilter
 
 class PostViewSet(viewsets.ModelViewSet):
 
     queryset = Essay.objects.all() # 모델에서 쿼리셋
-#   serializer_class = EssaySerializer
+    serializer_class = EssaySerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'body',) # 튜플로써 저장이 된다.
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
+
+        # 현재 request을 보낸 유저
+        # == self.request.user
+
+        # 현재 필터링해야한다.
+    def get_queryset(self):
+        qs = super().get_queryset() # super() 부모클래스라고 보면된다.
+        # 지지고 볶는 다면 생각하면 된다.
+
+        if self.request.user.is_authenticated: # 로그인이 되어 있다면
+            qs = qs.filter(author = self.request.user) # 필터 또는 exclude
+
+        else:
+            qs = qs.none() # 로그아웃이 되어 있다면
+
+        return qs
+
--- proj/urls.py
+++ proj/urls.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 from mystorage import urls
 
+from rest_framework import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mystorage.urls')),
+    path('api-auth/', include('rest_framework.urls')), # 필터링 과정을 만들어야 한다.
 ]
